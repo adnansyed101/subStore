0839a429c695025e2a2f17c7719407218f32875b
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-10 21:31:23 +0600

    show storeItems in store Page.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Grid } from "@mui/material";
+import storeItems from "../data/streamingServices.json";
+import { StoreItem } from "../components/StoreItem";
+
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <div className="px-5">
+      <h1 className="text-xl">Store</h1>
+      <Grid container spacing={2}>
+        {storeItems.map((item) => (
+          <Grid item xs={12} sm={6} lg={4} key={item.id}>
+            <StoreItem {...item} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
