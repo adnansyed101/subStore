fe51485ce044797c745cc196898cd66668982342
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-10 18:33:32 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="text-2xl">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
