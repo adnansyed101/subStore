9db0e45e7fd8d1e3c573301cb59e049457d826b4
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-10 20:19:43 +0600

    Create Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,33 @@
+import { Badge, Button } from "@mui/material";
+import { BsFillCartFill } from "react-icons/bs";
+import shoppingCart from "/shoppingCart.svg";
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <Link to="/" className="logo">
+        <img src={shoppingCart} alt="Shopping Cart" />
+        SubStore
+      </Link>
+      <div className="links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/store">Store</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <Badge badgeContent={5} color="error">
+          <Button color="primary" variant="outlined" className="rounded-full">
+            <BsFillCartFill />
+          </Button>
+        </Badge>
+      </div>
+    </div>
+  );
 };
