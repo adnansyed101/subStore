8fd42ddb3ac923de0e73b7a3622e482958e6ed1e
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-11 15:42:39 +0600

    added add to cart styles.
    used conditional rendering and various styles to display add to cart button if quantity is 0. And 'value' is in cart when quantity is more than 0 with two buttons beside.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -11,7 +18,7 @@ type StoreItemProps = {
 export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
   const quantity = 0;
   return (
-    <Card>
+    <Card sx={{ h: 100 }}>
       <CardMedia
         image={imgurl}
         title={name}
@@ -25,6 +32,38 @@ export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
           {formatCurrency(price)}
         </Typography>
       </CardContent>
+      <div className="mt-auto p-2">
+        {quantity === 0 ? (
+          <Button variant="contained" fullWidth={true} className="bg-blue-500">
+            + Add to Cart
+          </Button>
+        ) : (
+          <div className="flex items-center flex-col gap-2">
+            <div className="flex items-center justify-center gap-2">
+              <Button variant="contained" className="bg-blue-500" size="medium">
+                +
+              </Button>
+              <div>
+                <Typography variant="h6" component="span">
+                  {quantity}
+                </Typography>{" "}
+                in cart
+              </div>
+              <Button variant="contained" className="bg-blue-500" size="medium">
+                -
+              </Button>
+            </div>
+            <Button
+              variant="contained"
+              color="error"
+              className="bg-red-500"
+              size="small"
+            >
+              Remove
+            </Button>
+          </div>
+        )}
+      </div>
     </Card>
   );
 };
