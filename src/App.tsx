910e4a3fecf317ea24ce8c3aa1b8b9fb6ccab566
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-13 22:18:47 +0600

    wrap app with shoppingCartContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <ShoppingCartProvider>
+      <div className="container mx-auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ShoppingCartProvider>
   );
 }
 
