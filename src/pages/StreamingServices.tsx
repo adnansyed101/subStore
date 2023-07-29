db0e0c605555220dd7ab4ef414371aa440a475e1
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-29 18:03:48 +0600

    Update StreamingServices.tsx

--- src/pages/StreamingServices.tsx
+++ src/pages/StreamingServices.tsx
@@ -4,7 +4,7 @@ import { StoreItem } from "../components/StoreItem";
 
 export const StreamingServices = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 m-2">
       <h1 className="text-xl text-center py-2">Streaming Services</h1>
       <Grid container spacing={2}>
         {streamingServices.map((item) => (
