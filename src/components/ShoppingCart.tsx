f10654a9c338b78837e280a04eaee328077c6c40
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-07-29 15:53:47 +0600

    Create ShoppingCart.tsx

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,46 @@
+import { Box, Stack, SwipeableDrawer } from "@mui/material";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
+import { formatCurrency } from "../utilities/formatCurrency";
+import streamingService from "../data/streamingServices.json";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart, openCart, cartItems } = useShoppingCart();
+
+  const list = () => (
+    <Box width={350} px={1} role="presentation">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <Stack spacing={2}>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+        <div className="flex justify-end font-bold text-lg">
+          Total{" "}
+          {formatCurrency(
+            cartItems.reduce((total, cartItem) => {
+              const item = streamingService.find((i) => i.id === cartItem.id);
+              return total + (item?.price || 0) * cartItem.quantity;
+            }, 0)
+          )}
+        </div>
+      </Stack>
+    </Box>
+  );
+
+  return (
+    <>
+      <SwipeableDrawer
+        anchor="right"
+        open={isOpen}
+        onClose={closeCart}
+        onOpen={openCart}
+      >
+        {list()}
+      </SwipeableDrawer>
+    </>
+  );
+};
